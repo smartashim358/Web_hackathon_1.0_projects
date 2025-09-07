662487c217f356bc56b6ec2fa11cfaf3a4f6d680
smartashim358 <ashimnepali358@gmail.com>
2025-09-07 16:36:20 +0545

    Added hackathon project

---
+++ trio_fusion/script.js
@@ -0,0 +1,14 @@
+/*const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 50){
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
+    
+    const booKBtn = document.getElementById('book_btn');
+    booKBtn.addEventLisntener('click',()=>{
+    alert("Welcom to LuxuryStay Hotels. Please click on Book NOW");
+    });
+    */
